e5b51e134a917fab2cd6f2467e5ddd7904c20c99
jiayizx <jiayizx0427@gmail.com>
2025-10-08 03:11:05 -0400

    sync domain name

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,13 @@ const nextConfig: NextConfig = {
   experimental: {
     ppr: false,
     clientSegmentCache: false,
+  },
+  distDir: 'out',
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true
   }
 };
 
 export default nextConfig;
-
-module.exports = {
-  distDir: 'out',
-  output: "export",
-}
