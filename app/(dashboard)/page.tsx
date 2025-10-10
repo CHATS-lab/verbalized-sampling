0e4ec9bdd3a1ac458c1aa93e548a79f914ff264f
jiayizx <jiayizx0427@gmail.com>
2025-10-10 16:33:38 -0400

    sync changes

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { ArrowRight, CreditCard, Database, Calendar, User, Clock, Eye, Heart, ThumbsUp, Share2 } from 'lucide-react';
 
-import { Terminal } from './terminal';
+import { Terminal_Prompt } from './terminal_prompt';
 
 // Type definitions
 interface VisitorSession {
@@ -851,7 +851,7 @@ export default function HomePage() {
               </div> */}
             </div>
             <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-5 lg:flex lg:items-center">
-              <Terminal />
+              <Terminal_Prompt />
             </div>
           </div>
         </div>
